87d11489ccb715ac3e10075f5cbddfac4e54cf58
ezeta-03 <ing.sergio.zenteno@gmail.com>
2026-01-21 04:15:06 -0500

    FIX: Dashboard Ventas

--- src/layouts/VentasLayout.jsx
+++ src/layouts/VentasLayout.jsx
@@ -5,10 +5,10 @@ export default function VentasLayout() {
     { path: "/ventas", label: "Dashboard" },
     { path: "/ventas/clientes", label: "Clientes" },
     { path: "/ventas/productos", label: "Productos" },
-    { path: "/ventas/cotizaciones", label: "Cotizar" },
+    // { path: "/ventas/cotizaciones", label: "Cotizar" },
     { path: "/ventas/historial", label: "Mis Cotizaciones" },
     { path: "/ventas/cotizaciones-ventas", label: "Facturar" },
-    { path: "/ventas/actividad", label: "Actividad Cliente" },
+    // { path: "/ventas/actividad", label: "Actividad Cliente" },
 
 
   ];
